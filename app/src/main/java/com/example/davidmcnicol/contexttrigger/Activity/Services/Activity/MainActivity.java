a62cce56068c638bdfefe63f3d16e2934f3be441
David McNicol <colmcnicol@btinternet.com>
2016-03-29 13:02:08 +0100

    updated values

--- app/src/main/java/com/example/davidmcnicol/contexttrigger/Activity/Services/Activity/MainActivity.java
+++ app/src/main/java/com/example/davidmcnicol/contexttrigger/Activity/Services/Activity/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends Activity{
     private Boolean isBarOn = false;
     private Boolean isCalOn = false;
     private final Context context = MainActivity.this;
+    private Boolean test = true;
+    private Boolean test2 = false;
 
 
     @Override

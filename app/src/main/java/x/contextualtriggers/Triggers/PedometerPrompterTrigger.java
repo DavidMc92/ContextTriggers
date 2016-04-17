1873140ce6e9a2d5de9a4b143420e27882a3b7ea
Greg Patrick <greg.patrick.2013@uni.strath.ac.uk>
2016-04-17 15:26:23 +0100

    Fixed some conflicted code, pedometer target hard-coded for testing

--- app/src/main/java/x/contextualtriggers/Triggers/PedometerPrompterTrigger.java
+++ app/src/main/java/x/contextualtriggers/Triggers/PedometerPrompterTrigger.java
@@ -3,10 +3,7 @@ package x.contextualtriggers.Triggers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
 import android.util.Log;
->>>>>>> Correcting merge
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Pair;
@@ -16,10 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 import x.contextualtriggers.Application.NotificationSender;
-<<<<<<< HEAD
-=======
 import x.contextualtriggers.R;
->>>>>>> Correcting merge
 import x.contextualtriggers.MessageObjects.CalendarInfo;
 import x.contextualtriggers.MessageObjects.ICalendarInfo;
 import x.contextualtriggers.MessageObjects.IWeatherInfo;
@@ -27,58 +21,42 @@ import x.contextualtriggers.MessageObjects.WeatherType;
 import x.contextualtriggers.Services.CalendarService;
 import x.contextualtriggers.Services.StepCounterService;
 import x.contextualtriggers.Services.WeatherService;
-<<<<<<< HEAD
-=======
-
->>>>>>> Correcting merge
 
 // TODO
 public class PedometerPrompterTrigger extends BroadcastReceiver implements ITrigger {
     private final Context context;
     private static final int NOTIFICATION_ID = 64002;
     private int steps;
+    private Boolean hasNotified = false;
 
     // Required services for trigger conditions
     private IWeatherInfo currWeather;
     private ICalendarInfo currCalendar;
 
-    public PedometerPrompterTrigger(Context context){
+    public PedometerPrompterTrigger(Context context) {
         this.context = context;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-        Log.d(getClass().getSimpleName(), "Received geofence trigger!");
-        NotificationSender.sendNotification(context, 64002,
-                R.drawable.ic_directions_walk_white_18dp,
-                RouteRecommenderTrigger.class.getSimpleName(),
-                "You passed a geofence!");
-
->>>>>>> Correcting merge
-=======
->>>>>>> Tidying up PedometerPrompter code to include step_counter intent. Need to fix preference for step target still
         // Parse out intents
         // WEATHER
-        if(intent.getAction().equals(WeatherService.WEATHER_INTENT)){
+        if (intent.getAction().equals(WeatherService.WEATHER_INTENT)) {
             this.currWeather = intent.getParcelableExtra(WeatherService.WEATHER_DATA);
         }
 
         // CALENDAR
-        else if(intent.getAction().equals(CalendarService.CALENDAR_INTENT)){
+        else if (intent.getAction().equals(CalendarService.CALENDAR_INTENT)) {
             this.currCalendar = intent.getParcelableExtra(CalendarService.CALENDAR_DATA);
         }
 
         // PEDOMETER
-        if(intent.getAction().equals(StepCounterService.PEDOMETER_INTENT)) {
+        if (intent.getAction().equals(StepCounterService.PEDOMETER_INTENT)) {
             this.steps = intent.getIntExtra(StepCounterService.PEDOMETER_KEY, 0);
         }
 
         // Check if all needed info has been delivered
-        if(this.currWeather != null && this.currCalendar != null && this.steps != 0) {
+        if (this.currWeather != null && this.currCalendar != null && this.steps != 0) {
             boolean isSuitableWeather = this.currWeather.getWeather() == WeatherType.CLOUDS ||
                     this.currWeather.getWeather() == WeatherType.CLEAR;
 
@@ -86,32 +64,29 @@ public class PedometerPrompterTrigger extends BroadcastReceiver implements ITrig
                     new Date().getTime());
 
             // TODO: perform check for user to have met step target
+            int target = 100;
+            boolean hasMetTarget = steps >= target;
 
-            
             // Check weather, availability and step count
-            if(isSuitableWeather && isUserAvailable) { // & !metStepTarget
+            if (isSuitableWeather && isUserAvailable && !hasMetTarget) {
                 NotificationSender.sendNotification(context, NOTIFICATION_ID,
                         R.drawable.ic_pedometer_white,
                         PedometerPrompterTrigger.class.getSimpleName(),
-                        "Let's get that step count up!");
+                        "You've not met your target today!Let's get that step count up!");
+                hasNotified = true;
+            } else {
+                hasNotified = false;
             }
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> Correcting merge
     }
+
 
     @Override
     public List<Pair<Class<?>, Integer>> getDependentServices() {
         final List<Pair<Class<?>, Integer>> ret = new ArrayList<>();
-<<<<<<< HEAD
-=======
-
->>>>>>> Correcting merge
         ret.add(new Pair(WeatherService.class, 500));
         ret.add(new Pair(CalendarService.class, 1000));
-        //TODO Pedometer service dependence
+        ret.add(new Pair(StepCounterService.class, 500));
         return ret;
     }
 
@@ -121,17 +96,12 @@ public class PedometerPrompterTrigger extends BroadcastReceiver implements ITrig
                 new IntentFilter(WeatherService.WEATHER_INTENT));
         LocalBroadcastManager.getInstance(context).registerReceiver(this,
                 new IntentFilter(CalendarService.CALENDAR_INTENT));
-        // TODO Pedometer Intent Filter
+        LocalBroadcastManager.getInstance(context).registerReceiver(this,
+                new IntentFilter(StepCounterService.PEDOMETER_INTENT));
     }
 
     @Override
     public void unregisterReceivers(Context context) {
-<<<<<<< HEAD
         LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
-=======
-
-        LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
-
->>>>>>> Correcting merge
     }
 }
